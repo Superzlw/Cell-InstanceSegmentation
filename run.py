3e077a03c52fbf6a05c4718235d28070beb38a86
ZlWeng1993 <zlweng1993@163.com>
2022-01-18 11:08:43 +0100

    multi

--- run.py
+++ run.py
@@ -63,16 +63,16 @@ def main():
                     st.image(processed_imgs)
                     processed_filenames.append('ALL')
                     selected_option = []
-                    while selected_option == []:
-                        selected_option = st.multiselect("Select one or more options:",processed_filenames)
+                    selected_option = st.multiselect("Select one or more options:",processed_filenames)
                     if 'ALL' in selected_option:
                         processed_filenames.remove('ALL')
                         selected_option = processed_filenames
-                    res_lst = [processed_filename2res[selected_filename] for selected_filename in selected_option]
-                    res_out_df = pd.concat(res_lst)
-                    res_excel = to_excel(res_out_df)
-                    st.download_button(label='Download the Result(.xlxs)', data=res_excel,
-                       file_name='result.xlsx')
+                    if not selected_option == []:
+                        res_lst = [processed_filename2res[selected_filename] for selected_filename in selected_option]
+                        res_out_df = pd.concat(res_lst)
+                        res_excel = to_excel(res_out_df)
+                        st.download_button(label='Download the Result(.xlxs)', data=res_excel,
+                           file_name='result.xlsx')
             else:
                 pass
 if __name__ == '__main__':
